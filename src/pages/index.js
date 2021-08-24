17de7865ef3be7c948b4827419a01138b1b56790
Chris Odegard <c.odegard@gmail.com>
2021-08-24 01:11:03 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,182 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState, useCallback } from "react";
+import Carousel, { Modal, ModalGateway } from "react-images";
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/layout";
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+import { FocusZone } from '@fluentui/react/lib/FocusZone';
+import { List } from '@fluentui/react/lib/List';
+import { getTheme, mergeStyleSets } from '@fluentui/style-utilities';
+import { useConst } from '@fluentui/react-hooks';
 
-export default IndexPage
+const theme = getTheme();
+const { palette, fonts } = theme;
+const ROWS_PER_PAGE = 1;
+const MAX_ROW_HEIGHT = 200;
+const classNames = mergeStyleSets({
+  listGrid: {
+    overflow: 'hidden',
+    fontSize: 0,
+    position: 'relative',
+  },
+  listGridTile: {
+    textAlign: 'center',
+    outline: 'none',
+    position: 'relative',
+    float: 'left',
+    background: palette.neutralLighter,
+    selectors: {
+      'focus:after': {
+        content: '',
+        position: 'absolute',
+        left: 2,
+        right: 2,
+        top: 2,
+        bottom: 2,
+        boxSizing: 'border-box',
+        border: `1px solid ${palette.white}`,
+      },
+    },
+  },
+  listGridSizer: {
+    paddingBottom: '100%',
+  },
+  listGridPadder: {
+    position: 'absolute',
+    left: 5,
+    top: 2,
+    right: 5,
+    bottom: 2,
+  },
+  listGridLabel: {
+    background: 'rgba(0, 0, 0, 0.3)',
+    color: '#FFFFFF',
+    position: 'absolute',
+    padding: 10,
+    bottom: 0,
+    left: 0,
+    width: '100%',
+    fontSize: fonts.small.fontSize,
+    boxSizing: 'border-box',
+  },
+  listGridImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+  },
+});
+
+
+const GalleryPage = ({ data }) => {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+  const openLightbox = useCallback((event, { photo, index }) => {
+    setCurrentImage(index);
+    setViewerIsOpen(true);
+  }, []);
+
+  const closeLightbox = () => {
+    setCurrentImage(0);
+    setViewerIsOpen(false);
+  };
+
+
+
+  const photos = data.allImageSharp.edges.map(edge => ({
+    key: `photo-${edge.node.id}`,
+    title: edge.node.fluid.originalName,
+    src: edge.node.fluid.base64 ,
+  }));
+
+  const columnCount = React.useRef(0);
+  const rowHeight = React.useRef(0);
+
+  const getItemCountForPage = React.useCallback((itemIndex, surfaceRect) => {
+    if (itemIndex === 0) {
+      columnCount.current = Math.ceil(surfaceRect.width / MAX_ROW_HEIGHT);
+      rowHeight.current = Math.floor(surfaceRect.width / columnCount.current);
+    }
+    return columnCount.current * ROWS_PER_PAGE;
+  }, []);
+
+  const onRenderCell = React.useCallback((item, index) => {
+    return (
+      <div
+        className={classNames.listGridTile}
+        data-is-focusable
+        style={{
+          width: 100 / columnCount.current + '%',
+        }}
+        onClick={(e) => { openLightbox(e, { item, index }) }}
+      >
+        <div className={classNames.listGridSizer}>
+          <div className={classNames.listGridPadder}>
+            {/* <Img fluid={item.fluid} /> */}
+            <img src={item.srcSet} className={classNames.listGridImage} />
+            <span className={classNames.listGridLabel}>{item.title}</span>
+          </div>
+        </div>
+      </div>
+    );
+  }, []);
+
+  const getPageHeight = React.useCallback(() => {
+    return rowHeight.current * ROWS_PER_PAGE;
+  }, []);
+
+  return (
+    <Layout>
+      <FocusZone>
+        <List
+          className={classNames.listGrid}
+          items={photos}
+          getItemCountForPage={getItemCountForPage}
+          getPageHeight={getPageHeight}
+          renderedWindowsAhead={1}
+          onRenderCell={onRenderCell}
+        />
+      </FocusZone>
+      <ModalGateway>
+        {viewerIsOpen ? (
+          <Modal onClose={closeLightbox}>
+            <Carousel
+              currentIndex={currentImage}
+              views={photos.map(x => ({
+                ...x,
+                // srcset: x.srcSet,
+                // caption: x.title
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
+    </Layout>
+  );
+};
+
+export const pageQuery = graphql`
+query {
+  allImageSharp(limit: 50, skip: 2) {
+    edges {
+      node {
+        id
+        original {
+          height
+          width
+          src
+        }
+        fluid {
+          base64
+          srcSet
+          originalImg
+          originalName
+        }
+      }
+    }
+  }
+}`;
+
+export default GalleryPage;
